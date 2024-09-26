ba675ac0c315d2816f41c349a08909389c453529
Alexis San Javier <alexissj16@knights.ucf.edu>
2024-09-26 19:53:50 -0400

    created React Native app with TypeScript using Expo

---
+++ client/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App: React.FC = () => {
+	return (
+		<View>
+			<Text>Ping up</Text>
+		</View>
+	);
+};
+
+export default App;
